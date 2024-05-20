c17644005aca401fb285f90c7c24cddfd89b571a
Jordla <jlscoding@gmail.com>
2024-05-20 16:57:51 -0600

    Add slow mode player feature

--- Player.gd
+++ Player.gd
@@ -3,10 +3,21 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 
-func get_input():
+		
+func move_character():
 	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down") # This is a vector 2D
-	velocity = input_direction * SPEED
-
+	
+	
+	var slow_mode = false
+	if Input.is_action_pressed("slow_mode"):
+		slow_mode = true
+		print(slow_mode)
+		
+		
+	if slow_mode == true:
+		velocity = (input_direction * SPEED) / 2
+	else: 
+		velocity = input_direction * SPEED
 func _physics_process(delta):
-	get_input()
+	move_character()
 	move_and_slide()
