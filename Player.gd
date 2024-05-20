4efafb4fa256b7d4f10b0d1616e484323255b6cb
Jordla <jlscoding@gmail.com>
2024-05-20 17:41:16 -0600

    Streamline player movement logic

--- Player.gd
+++ Player.gd
@@ -1,23 +1,24 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED: int = 300.0
 
+var slow_mode: bool = false
 		
 func move_character():
 	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down") # This is a vector 2D
-	
-	
-	var slow_mode = false
-	if Input.is_action_pressed("slow_mode"):
-		slow_mode = true
-		print(slow_mode)
-		
-		
 	if slow_mode == true:
 		velocity = (input_direction * SPEED) / 2
 	else: 
 		velocity = input_direction * SPEED
+
+func is_slow_mode():
+	if Input.is_action_pressed("slow_mode"):
+		slow_mode = true
+	else:
+		slow_mode = false
+
 func _physics_process(delta):
+	is_slow_mode()
 	move_character()
 	move_and_slide()
