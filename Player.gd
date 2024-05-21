f14eb59a60ff1f12f6bd4c3aca7758ea34848797
Jordla <jlscoding@gmail.com>
2024-05-20 18:36:52 -0600

    Add player ability to shoot projectiles

--- Player.gd
+++ Player.gd
@@ -1,9 +1,15 @@
 extends CharacterBody2D
 
-
 const SPEED: int = 300.0
 
 var slow_mode: bool = false
+var is_shooting: bool = false
+var shoot_timer: float = 0.0
+
+@export var Bullet: PackedScene
+@onready var marker_2d = $Marker2D
+
+
 		
 func move_character():
 	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down") # This is a vector 2D
@@ -18,7 +24,32 @@ func is_slow_mode():
 	else:
 		slow_mode = false
 
+func shoot():
+	var bullet = Bullet.instantiate()
+	add_child(bullet)
+	bullet.transform = $Marker2D.transform
+	
+func _on_bullet_timer_timeout():
+	shoot()
+	
+func _process(delta):
+	if Input.is_action_pressed("shoot"):
+		if is_shooting:
+			shoot_timer += delta
+			if shoot_timer > 0.04:
+				shoot()
+				shoot_timer = 0
+		else:
+			shoot()
+			is_shooting = true
+			shoot_timer = 0
+	if Input.is_action_just_released("shoot"):
+		is_shooting = false
+
+
 func _physics_process(delta):
 	is_slow_mode()
 	move_character()
 	move_and_slide()
+
+
---
+++ bullet.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+var bullet_speed: int = -1000
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	position += transform.y * bullet_speed * delta
